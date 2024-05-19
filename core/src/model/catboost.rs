6ca6fd08f7c33040200f68c3398c464540050665
gagansingh894 <gds31.gagandeepsingh@gmail.com>
2024-05-19 15:59:04 +0100

    feat: implement model loader for catboost

--- core/src/model/catboost.rs
+++ core/src/model/catboost.rs
@@ -1,10 +1,16 @@
 use crate::model::predictor::Predictor;
 
-pub struct Catboost {}
+use catboost_rs;
+
+pub struct Catboost {
+    model: catboost_rs::Model,
+}
 
 impl Catboost {
-    pub fn load() -> Self {
-        todo!()
+    pub fn load(path: &str) -> anyhow::Result<Self> {
+        let model =
+            catboost_rs::Model::load(path).expect("failed to load catboost model from .cbm file");
+        Ok(Catboost { model })
     }
 }
 
@@ -15,4 +21,33 @@ impl Predictor for Catboost {
 }
 
 #[cfg(test)]
-mod tests {}
+mod tests {
+    use super::*;
+
+    #[test]
+    fn successfully_load_catboost_regressor_model() {
+        let path = "tests/model_artefacts/catboost_regressor";
+        let model = Catboost::load(path);
+
+        // assert the result is ok
+        assert!(model.is_ok())
+    }
+
+    #[test]
+    fn successfully_load_catboost_binary_classification_model() {
+        let path = "tests/model_artefacts/catboost_binary";
+        let model = Catboost::load(path);
+
+        // assert the result is ok
+        assert!(model.is_ok())
+    }
+
+    #[test]
+    fn successfully_load_catboost_multiclass_classification_model() {
+        let path = "tests/model_artefacts/catboost_multiclass";
+        let model = Catboost::load(path);
+
+        // assert the result is ok
+        assert!(model.is_ok())
+    }
+}
