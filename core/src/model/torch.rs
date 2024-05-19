3480a32a00dfcc03abb78291147902b4642b3d80
gagansingh894 <gds31.gagandeepsingh@gmail.com>
2024-05-19 15:59:30 +0100

    feat: implement model loader for torch

--- core/src/model/torch.rs
+++ core/src/model/torch.rs
@@ -1,10 +1,15 @@
 use crate::model::predictor::Predictor;
 
-pub struct Torch {}
+use tch::CModule;
+
+pub struct Torch {
+    model: CModule,
+}
 
 impl Torch {
-    pub fn load() -> Self {
-        todo!()
+    pub fn load(path: &str) -> anyhow::Result<Self> {
+        let model = CModule::load(path).expect("failed to load pytorch model from file");
+        Ok(Torch { model })
     }
 }
 
@@ -15,4 +20,24 @@ impl Predictor for Torch {
 }
 
 #[cfg(test)]
-mod tests {}
+mod tests {
+    use super::*;
+
+    #[test]
+    fn successfully_load_pytorch_regression_model() {
+        let path = "tests/model_artefacts/californiahousing_pytorch.pt";
+        let model = Torch::load(path);
+
+        // assert the result is Ok
+        assert!(model.is_ok())
+    }
+
+    #[test]
+    fn successfully_load_pytorch_multiclass_classification_model() {
+        let model_path = "tests/model_artefacts/mnist_pytorch.pt";
+        let model = Torch::load(model_path);
+
+        // assert the result is Ok
+        assert!(model.is_ok())
+    }
+}
