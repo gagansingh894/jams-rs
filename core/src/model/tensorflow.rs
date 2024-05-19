546391bc03dd394d67c6a84a8bd22ead5869e681
gagansingh894 <gds31.gagandeepsingh@gmail.com>
2024-05-19 15:59:24 +0100

    feat: implement model loader for tensorflow

--- core/src/model/tensorflow.rs
+++ core/src/model/tensorflow.rs
@@ -1,12 +1,20 @@
 use crate::model::predictor::Predictor;
+use tensorflow::{Graph, SavedModelBundle, SessionOptions};
 
-pub struct Tensorflow {}
+pub struct Tensorflow {
+    bundle: SavedModelBundle,
+}
 
 impl Tensorflow {
-    pub fn load() -> Self {
-        todo!()
+    pub fn load(model_dir: &str) -> anyhow::Result<Self> {
+        const MODEL_TAG: &str = "serve";
+        let mut graph = Graph::new();
+        let bundle =
+            SavedModelBundle::load(&SessionOptions::new(), &[MODEL_TAG], &mut graph, model_dir)
+                .expect("failed to load tensorflow savedmodel");
+        Ok(Tensorflow { bundle })
     }
- }
+}
 
 impl Predictor for Tensorflow {
     fn predict() {
@@ -15,4 +23,24 @@ impl Predictor for Tensorflow {
 }
 
 #[cfg(test)]
-mod tests {}
+mod tests {
+    use super::*;
+
+    #[test]
+    fn successfully_load_tensorflow_regression_model() {
+        let model_dir = "tests/model_artefacts/autompg_tensorflow";
+        let model = Tensorflow::load(model_dir);
+
+        // assert the result is Ok
+        assert!(model.is_ok())
+    }
+
+    #[test]
+    fn successfully_load_tensorflow_multi_classification_model() {
+        let model_dir = "tests/model_artefacts/mnist_tensorflow";
+        let model = Tensorflow::load(model_dir);
+
+        // assert the result is Ok
+        assert!(model.is_ok())
+    }
+}
