1920388da27e0b39efd34624b939c2ed4bb6091c
gagansingh894 <gds31.gagandeepsingh@gmail.com>
2024-05-19 15:59:14 +0100

    feat: implement model loader for lightgbm

--- core/src/model/lightgbm.rs
+++ core/src/model/lightgbm.rs
@@ -1,10 +1,15 @@
 use crate::model::predictor::Predictor;
+use lgbm;
 
-pub struct LightGBM {}
+pub struct LightGBM {
+    booster: lgbm::Booster,
+}
 
 impl LightGBM {
-    pub fn load() -> Self {
-        todo!()
+    pub fn load(path: &str) -> anyhow::Result<Self> {
+        let model = lgbm::Booster::from_file(path.as_ref())
+            .expect("failed to load lightGBM model fro  file");
+        Ok(LightGBM { booster: model.0 })
     }
 }
 
@@ -15,4 +20,42 @@ impl Predictor for LightGBM {
 }
 
 #[cfg(test)]
-mod tests {}
+mod tests {
+    use super::*;
+
+    #[test]
+    fn successfully_load_lightgbm_regressor_model() {
+        let path = "tests/model_artefacts/lgbm_reg.txt";
+        let model = LightGBM::load(path);
+
+        // assert the result is ok
+        assert!(model.is_ok())
+    }
+
+    #[test]
+    fn successfully_load_lightgbm_binary_classifier_model() {
+        let path = "tests/model_artefacts/lgbm_binary.txt";
+        let model = LightGBM::load(path);
+
+        // assert the result is ok
+        assert!(model.is_ok())
+    }
+
+    #[test]
+    fn successfully_load_lightgbm_xentropy_binary_classifier_model() {
+        let path = "tests/model_artefacts/lgbm_xen_binary.txt";
+        let model = LightGBM::load(path);
+
+        // assert the result is ok
+        assert!(model.is_ok())
+    }
+
+    #[test]
+    fn successfully_load_lightgbm_xentropy_probability_classifier_model() {
+        let path = "tests/model_artefacts/lgbm_binary.txt";
+        let model = LightGBM::load(path);
+
+        // assert the result is ok
+        assert!(model.is_ok())
+    }
+}
