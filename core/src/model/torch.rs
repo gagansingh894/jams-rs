830db0b89bc4d3aa6341d41cd399ce89a50c5079
gagansingh894 <gds31.gagandeepsingh@gmail.com>
2024-05-23 21:43:26 +0100

    chore: disable torch

--- core/src/model/torch.rs
+++ core/src/model/torch.rs
@@ -1,43 +1,44 @@
-use crate::model::predictor::{ModelInput, Output, Predictor};
-
-use tch::CModule;
-
-pub struct Torch {
-    model: CModule,
-}
-
-impl Torch {
-    pub fn load(path: &str) -> anyhow::Result<Self> {
-        let model = CModule::load(path).expect("failed to load pytorch model from file");
-        Ok(Torch { model })
-    }
-}
-
-impl Predictor for Torch {
-    fn predict(&self, input: ModelInput) -> anyhow::Result<Output> {
-        todo!()
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn successfully_load_pytorch_regression_model() {
-        let path = "tests/model_artefacts/californiahousing_pytorch.pt";
-        let model = Torch::load(path);
-
-        // assert the result is Ok
-        assert!(model.is_ok())
-    }
-
-    #[test]
-    fn successfully_load_pytorch_multiclass_classification_model() {
-        let model_path = "tests/model_artefacts/mnist_pytorch.pt";
-        let model = Torch::load(model_path);
-
-        // assert the result is Ok
-        assert!(model.is_ok())
-    }
-}
+// todo: uncomment when cicd passes on linux
+// use crate::model::predictor::{ModelInput, Output, Predictor};
+//
+// use tch::CModule;
+//
+// pub struct Torch {
+//     model: CModule,
+// }
+//
+// impl Torch {
+//     pub fn load(path: &str) -> anyhow::Result<Self> {
+//         let model = CModule::load(path).expect("failed to load pytorch model from file");
+//         Ok(Torch { model })
+//     }
+// }
+//
+// impl Predictor for Torch {
+//     fn predict(&self, input: ModelInput) -> anyhow::Result<Output> {
+//         todo!()
+//     }
+// }
+//
+// #[cfg(test)]
+// mod tests {
+//     use super::*;
+//
+//     #[test]
+//     fn successfully_load_pytorch_regression_model() {
+//         let path = "tests/model_artefacts/californiahousing_pytorch.pt";
+//         let model = Torch::load(path);
+//
+//         // assert the result is Ok
+//         assert!(model.is_ok())
+//     }
+//
+//     #[test]
+//     fn successfully_load_pytorch_multiclass_classification_model() {
+//         let model_path = "tests/model_artefacts/mnist_pytorch.pt";
+//         let model = Torch::load(model_path);
+//
+//         // assert the result is Ok
+//         assert!(model.is_ok())
+//     }
+// }
