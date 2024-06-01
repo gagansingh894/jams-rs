8d75d6792528b3a9172bf0506fb707d580b24750
gagansingh894 <gds31.gagandeepsingh@gmail.com>
2024-06-01 22:45:55 +0100

    feat: helper method for creating model inputs with specified names

--- core/src/model/test_utils.rs
+++ core/src/model/test_utils.rs
@@ -48,3 +48,49 @@ pub fn create_model_inputs(
 
     ModelInput::from_hashmap(model_input).unwrap()
 }
+
+pub fn create_model_inputs_with_names(
+    numeric_features_names: Vec<String>,
+    string_features_names: Vec<String>,
+    size: usize,
+) -> ModelInput {
+    let mut model_input: HashMap<FeatureName, Values> = HashMap::new();
+    let mut rng = rand::thread_rng();
+    let random_string_values: Vec<String> = vec![
+        "a".to_string(),
+        "b".to_string(),
+        "c".to_string(),
+        "d".to_string(),
+        "e".to_string(),
+    ];
+
+
+    // create string features
+    for feature_name in string_features_names {
+        let mut string_features: Vec<Value> = Vec::new();
+
+        for _ in 0..size {
+            let value = random_string_values
+                .get(rng.gen_range(0..random_string_values.len()))
+                .unwrap()
+                .to_string();
+            string_features.push(Value::String(value));
+        }
+
+        model_input.insert(feature_name, Values(string_features));
+    }
+
+    // create numeric features
+    for feature_name in numeric_features_names {
+        let mut number_features: Vec<Value> = Vec::new();
+
+        for _ in 0..size {
+            let value = rng.gen::<f32>();
+            number_features.push(Value::Float(value));
+        }
+
+        model_input.insert(feature_name, Values(number_features));
+    }
+
+    ModelInput::from_hashmap(model_input).unwrap()
+}
