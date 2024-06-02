4c4aeb18a68b9870e7a499d04ae9157acc000c6d
gagansingh894 <gds31.gagandeepsingh@gmail.com>
2024-06-03 00:34:12 +0100

    feat: update signature

--- jams-core/src/model_store/storage.rs
+++ jams-core/src/model_store/storage.rs
@@ -6,7 +6,7 @@ pub type ModelName = String;
 /// Trait representing a storage system for machine learning models.
 pub trait Storage {
     /// Fetches all available models from the storage.
-    fn fetch_models(&self);
+    fn fetch_models(&self) -> anyhow::Result<()>;
 
     /// Retrieves a specific machine learning/deep learning model by its name.
     fn get_model(&self, _: ModelName) -> Arc<dyn Predictor>;
