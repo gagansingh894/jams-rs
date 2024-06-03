04079b6309f4c489f2e27f494c351901b38b61ba
gagansingh894 <gds31.gagandeepsingh@gmail.com>
2024-06-03 20:47:56 +0100

    chore: update build script

--- jams-cli/build.rs
+++ jams-cli/build.rs
@@ -1,3 +1,13 @@
+use std::env;
+
 fn main() {
-    println!("cargo:rustc-link-arg-bin=jams-cli=-Wl,-rpath,/usr/local/lib");
+    let target_os = env::var("CARGO_CFG_TARGET_OS").unwrap();
+
+    if target_os == "macos" {
+        println!("cargo:rustc-link-arg-bin=jams-cli=-Wl,-rpath,@loader_path/../lib");
+        println!("cargo:rustc-link-arg-bin=jams-cli=-Wl,-rpath,/usr/local/lib");
+    } else if target_os == "linux" {
+        println!("cargo:rustc-link-arg-bin=jams-cli=-Wl,-rpath,$ORIGIN/../lib");
+        println!("cargo:rustc-link-arg-bin=jams-cli=-Wl,-rpath,/usr/local/lib");
+    }
 }
