3d9ff40973c293af171456393b6bfeb48fbbd579
gagansingh894 <gds31.gagandeepsingh@gmail.com>
2024-06-04 11:23:50 +0100

    chore: search dir

--- jams-cli/build.rs
+++ jams-cli/build.rs
@@ -10,4 +10,6 @@ fn main() {
         println!("cargo:rustc-link-arg-bin=jams-cli=-Wl,-rpath,$ORIGIN/../lib");
         println!("cargo:rustc-link-arg-bin=jams-cli=-Wl,-rpath,/usr/local/lib");
     }
+
+    println!("cargo:rustc-link-search=native=/usr/local/lib");
 }
