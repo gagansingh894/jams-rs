fd5e81db6d92ae8eb425c665c44e46cc17aaa216
gagansingh894 <gds31.gagandeepsingh@gmail.com>
2024-06-04 23:20:42 +0100

    feat: add cli for server

--- jams-serve/src/main.rs
+++ jams-serve/src/main.rs
@@ -1,36 +1,63 @@
+mod router;
 mod service;
-use crate::service::{healthcheck, predict, root};
 
-use axum::{
-    routing::{get, post},
-    Router,
-};
-use jams_core::{manager::Manager, model_store::local::LocalModelStore};
-use std::sync::Arc;
+use crate::router::build_router;
+use clap::{Args, Parser, Subcommand};
 
+/// CLI for starting an J.A.M.S
+#[derive(Parser, Debug)]
+#[clap(
+    name = "J.A.M.S - Just Another Model Server",
+    version = "0.1.0",
+    author = "Gagandeep Singh",
+    about = "Starts the server with specified model directory and optional port number. "
+)]
+struct Cli {
+    #[clap(subcommand)]
+    cmd: Commands,
+}
+
+#[derive(Subcommand, Debug)]
+enum Commands {
+    /// Start the Axum server
+    #[clap(name = "start")]
+    Start(CommandArgs),
+}
+
+#[derive(Args, Debug)]
+struct CommandArgs {
+    /// Path to the directory containing models
+    #[clap(long)]
+    model_dir: Option<String>,
+
+    /// Port number (default: 3000)
+    #[clap(long)]
+    port: Option<u16>,
+}
+
+#[cfg(not(tarpaulin_include))]
 #[tokio::main]
-async fn main() {
-    // initialize tracing
-    tracing_subscriber::fmt::init();
+async fn main() -> anyhow::Result<()> {
+    let cli = Cli::parse();
 
-    // setup model store
-    let model_dir = "assets/model_storage/local_model_store".to_string();
-    let model_store = LocalModelStore::new(model_dir).unwrap();
-    let manager = Manager::new(Arc::new(model_store)).unwrap();
+    match cli.cmd {
+        Commands::Start(args) => {
+            start_server(args).await;
+            Ok(())
+        },
+    }
+}
 
-    // shared state
-    let shared_state = Arc::new(manager);
+async fn start_server(args: CommandArgs) {
+    let model_dir = match args.model_dir {
+        Some(dir) => dir,
+        None => {
+            eprintln!("Error: 'model-dir' argument is required.");
+            return;
+        }
+    };
 
-    // API routes
-    let api_routes = Router::new().route("/predict", post(predict));
-
-    // build server
-    let app = Router::new()
-        .route("/", get(root))
-        .route("/healthcheck", get(healthcheck))
-        .nest("/api", api_routes)
-        .with_state(shared_state);
-
+    let app = build_router(model_dir).expect("failed to build router");
     // run our app with hyper, listening globally on port 3000
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000")
         .await
---
+++ jams-serve/src/router.rs
@@ -0,0 +1,29 @@
+use crate::service::{healthcheck, predict, root};
+use axum::routing::{get, post};
+use axum::Router;
+use jams_core::manager::Manager;
+use jams_core::model_store::local::LocalModelStore;
+use std::sync::Arc;
+
+pub fn build_router(model_dir: String) -> anyhow::Result<Router> {
+    // initialize tracing
+    tracing_subscriber::fmt::init();
+
+    // setup model store
+    // let model_dir = "assets/model_storage/local_model_store".to_string();
+    let model_store = LocalModelStore::new(model_dir).expect("failed to create model store");
+    let manager = Manager::new(Arc::new(model_store)).expect("failed to initialize manage");
+
+    // shared state
+    let shared_state = Arc::new(manager);
+
+    // API routes
+    let api_routes = Router::new().route("/predict", post(predict));
+
+    // build router
+    Ok(Router::new()
+        .route("/", get(root))
+        .route("/healthcheck", get(healthcheck))
+        .nest("/api", api_routes)
+        .with_state(shared_state))
+}
