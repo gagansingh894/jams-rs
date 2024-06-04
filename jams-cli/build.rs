a6e7d85b04f1cfe10a7131f0c7e6bebdd170dd58
gagansingh894 <gds31.gagandeepsingh@gmail.com>
2024-06-04 14:02:10 +0100

    chore: bump version

--- jams-cli/build.rs
+++ jams-cli/build.rs
@@ -4,7 +4,7 @@ fn main() {
     let target_os = env::var("CARGO_CFG_TARGET_OS").unwrap();
 
     if target_os == "macos" {
-        println!("cargo:rustc-link-arg-bin=jams-cli=-Wl,-rpath,@loader_path/../lib");
+        // println!("cargo:rustc-link-arg-bin=jams-cli=-Wl,-rpath,@loader_path/../lib");
         println!("cargo:rustc-link-arg-bin=jams-cli=-Wl,-rpath,@executable_path/../lib");
         println!("cargo:rustc-link-arg-bin=jams-cli=-Wl,-rpath,/usr/local/lib");
     } else if target_os == "linux" {
