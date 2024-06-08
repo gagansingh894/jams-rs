2cebbc3c006292cdb7b7ed05ee2593bcfe9c645f
gagansingh894 <gds31.gagandeepsingh@gmail.com>
2024-06-08 23:43:48 +0100

    feat: add load_model helper function and tests

--- jams-core/src/model_store/storage.rs
+++ jams-core/src/model_store/storage.rs
@@ -1,9 +1,9 @@
-use crate::model::predictor::Predictor;
-use crate::model::frameworks::{ModelFramework, CATBOOST, LIGHTGBM, PYTORCH, TENSORFLOW, TORCH};
-use dashmap::mapref::one::Ref;
-use std::sync::Arc;
-use serde::Serialize;
 use crate::model;
+use crate::model::frameworks::{ModelFramework, CATBOOST, LIGHTGBM, PYTORCH, TENSORFLOW, TORCH};
+use crate::model::predictor::Predictor;
+use dashmap::mapref::one::Ref;
+use serde::Serialize;
+use std::sync::Arc;
 
 pub type ModelName = String;
 
@@ -27,7 +27,6 @@ pub trait Storage: Send + Sync + 'static {
     /// Removes a specific machine learning/deep learning model by its name.
     fn delete_model(&self, model_name: ModelName) -> anyhow::Result<()>;
 }
-
 
 /// Represents a machine learning model.
 ///
@@ -98,10 +97,7 @@ impl Model {
             last_updated,
         };
 
-        Model {
-            predictor,
-            info,
-        }
+        Model { predictor, info }
     }
 }
 
@@ -122,30 +118,226 @@ impl Model {
 /// * The model framework is unsupported.
 /// * The model fails to load due to an internal error specific to the framework.
 ///
-pub fn load_model(model_framework: ModelFramework, model_path: &str) -> anyhow::Result<Arc<dyn Predictor>> {
+pub fn load_predictor(
+    model_framework: ModelFramework,
+    model_path: &str,
+) -> anyhow::Result<Arc<dyn Predictor>> {
     if model_framework == TENSORFLOW {
         match model::tensorflow::Tensorflow::load(model_path) {
-            Ok(predictor) => {
-                Ok(Arc::new(predictor))
+            Ok(predictor) => Ok(Arc::new(predictor)),
+            Err(e) => {
+                anyhow::bail!("Failed to load Tensorflow model: {}", e)
             }
-            Err(e) => { anyhow::bail!("Failed to load Tensorflow model: {}", e) }
         }
     } else if (model_framework == TORCH) || (model_framework == PYTORCH) {
         match model::torch::Torch::load(model_path) {
-            Ok(predictor) => { Ok(Arc::new(predictor)) }
-            Err(e) => { anyhow::bail!("Failed to load Torch model: {}", e) }
+            Ok(predictor) => Ok(Arc::new(predictor)),
+            Err(e) => {
+                anyhow::bail!("Failed to load Torch model: {}", e)
+            }
         }
     } else if model_framework == CATBOOST {
         match model::catboost::Catboost::load(model_path) {
-            Ok(predictor) => { Ok(Arc::new(predictor)) }
-            Err(e) => { anyhow::bail!("Failed to load Catboost model: {}", e) }
+            Ok(predictor) => Ok(Arc::new(predictor)),
+            Err(e) => {
+                anyhow::bail!("Failed to load Catboost model: {}", e)
+            }
         }
     } else if model_framework == LIGHTGBM {
         match model::lightgbm::LightGBM::load(model_path) {
-            Ok(predictor) => { Ok(Arc::new(predictor)) }
-            Err(e) => { anyhow::bail!("Failed to load LightGBM model: {}", e) }
+            Ok(predictor) => Ok(Arc::new(predictor)),
+            Err(e) => {
+                anyhow::bail!("Failed to load LightGBM model: {}", e)
+            }
         }
     } else {
         anyhow::bail!("unsupported model framework: {}", model_framework)
+    }
+}
+
+/// Extracts the model framework from the given model path.
+///
+/// This function checks the provided model path for the presence of specific framework identifiers and returns the corresponding `ModelFramework` enum if a match is found.
+///
+/// # Arguments
+///
+/// * `model_path` - A string containing the path to the model file.
+///
+/// # Returns
+///
+/// This function returns an `Option<ModelFramework>`:
+/// * `Some(ModelFramework)` if a matching framework identifier is found in the model path.
+/// * `None` if no matching framework identifier is found.
+///
+pub fn extract_framework_from_path(model_path: String) -> Option<ModelFramework> {
+    if model_path.contains(TENSORFLOW) {
+        Some(TENSORFLOW)
+    } else if model_path.contains(PYTORCH) {
+        Some(PYTORCH)
+    } else if model_path.contains(TORCH) {
+        Some(TORCH)
+    } else if model_path.contains(CATBOOST) {
+        Some(CATBOOST)
+    } else if model_path.contains(LIGHTGBM) {
+        Some(LIGHTGBM)
+    } else {
+        None
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn successfully_create_model_artefact() {
+        // Setup
+        let model_name = "pytorch-my_awesome_californiahousing_model".to_string(); // notice that model name is exactly the same in path
+        let path =
+            "tests/model_storage/local_model_store/pytorch-my_awesome_californiahousing_model.pt";
+        let framework = PYTORCH;
+        let timestamp = chrono::Utc::now();
+        let predictor = model::torch::Torch::load(path).unwrap();
+
+        // Create
+        let model = Model::new(
+            Arc::new(predictor),
+            model_name.clone(),
+            framework,
+            path.to_string(),
+            timestamp.to_rfc3339(),
+        );
+
+        // Assert
+        assert_eq!(model_name, model.info.name);
+        assert_eq!(path, model.info.path.as_str());
+        assert_eq!(framework, model.info.framework);
+        assert_eq!(timestamp.to_rfc3339(), model.info.last_updated);
+    }
+
+    #[test]
+    fn successfully_load_tensorflow_framework_model() {
+        let path = "tests/model_storage/local_model_store/tensorflow-my_awesome_penguin_model";
+        let framework = TENSORFLOW;
+
+        // Load Model
+        let model = load_predictor(framework, path);
+
+        // Assert
+        assert!(model.is_ok());
+    }
+
+    #[test]
+    fn successfully_load_torch_framework_model() {
+        let path = "tests/model_storage/local_model_store/torch-my_awesome_penguin_model.pt";
+        let framework = TORCH;
+
+        // Load Model
+        let model = load_predictor(framework, path);
+
+        // Assert
+        assert!(model.is_ok());
+    }
+
+    #[test]
+    fn successfully_load_pytorch_framework_model() {
+        let path =
+            "tests/model_storage/local_model_store/pytorch-my_awesome_californiahousing_model.pt";
+        let framework = PYTORCH;
+
+        // Load Model
+        let model = load_predictor(framework, path);
+
+        // Assert
+        assert!(model.is_ok());
+    }
+
+    #[test]
+    fn successfully_load_lightgbm_model() {
+        let path = "tests/model_storage/local_model_store/lightgbm-my_awesome_binary_model_2.txt";
+        let framework = LIGHTGBM;
+
+        // Load Model
+        let model = load_predictor(framework, path);
+
+        // Assert
+        assert!(model.is_ok());
+    }
+
+    #[test]
+    fn successfully_load_catboost_model() {
+        let path = "tests/model_storage/local_model_store/catboost-titanic_model";
+        let framework = CATBOOST;
+
+        // Load Model
+        let model = load_predictor(framework, path);
+
+        // Assert
+        assert!(model.is_ok());
+    }
+
+    #[test]
+    fn successfully_extract_framework_from_path_when_tensorflow_framework() {
+        let path = "model/directory/tensorflow-my_model";
+
+        let result = extract_framework_from_path(path.to_string());
+
+        // assert
+        assert!(result.is_some());
+        assert_eq!(result.unwrap(), TENSORFLOW)
+    }
+
+    #[test]
+    fn successfully_extract_framework_from_path_when_torch_framework() {
+        let path = "model/directory/torch-my_model";
+
+        let result = extract_framework_from_path(path.to_string());
+
+        // assert
+        assert!(result.is_some());
+        assert_eq!(result.unwrap(), TORCH)
+    }
+
+    #[test]
+    fn successfully_extract_framework_from_path_when_pytorch_framework() {
+        let path = "model/directory/pytorch-my_model";
+
+        let result = extract_framework_from_path(path.to_string());
+
+        // assert
+        assert!(result.is_some());
+        assert_eq!(result.unwrap(), PYTORCH)
+    }
+
+    #[test]
+    fn successfully_extract_framework_from_path_when_lightgbm_framework() {
+        let path = "model/directory/lightgbm-my_model";
+
+        let result = extract_framework_from_path(path.to_string());
+
+        // assert
+        assert!(result.is_some());
+        assert_eq!(result.unwrap(), LIGHTGBM)
+    }
+
+    #[test]
+    fn successfully_extract_framework_from_path_when_catboost_framework() {
+        let path = "model/directory/catboost-my_model";
+
+        let result = extract_framework_from_path(path.to_string());
+
+        // assert
+        assert!(result.is_some());
+        assert_eq!(result.unwrap(), CATBOOST)
+    }
+
+    #[test]
+    fn fails_to_extract_framework_from_path_when_unknown_framework() {
+        let path = "model/directory/fbprophet-my_model";
+
+        let result = extract_framework_from_path(path.to_string());
+
+        // assert
+        assert!(result.is_none());
     }
 }
