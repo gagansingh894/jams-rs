c6bec1b67cb6d3296db6d118c1fe8e7c213b202a
gagansingh894 <gds31.gagandeepsingh@gmail.com>
2024-06-12 21:23:33 +0100

    chore: uncomment tests

--- jams-serve/tests/api/helper.rs
+++ jams-serve/tests/api/helper.rs
@@ -1,11 +1,11 @@
-// use axum::Router;
-// use jams_serve::server::build_router;
-//
-// pub fn test_router() -> Router {
-//     // we will not set a model for testing purpose
-//     // this will start the model server without any models loaded
-//     let model_dir = "".to_string();
-//     let worker_pool_threads = 1;
-//
-//     build_router(model_dir, worker_pool_threads).unwrap()
-// }
+use axum::Router;
+use jams_serve::server::build_router;
+
+pub fn test_router() -> Router {
+    // we will not set a model for testing purpose
+    // this will start the model server without any models loaded
+    let model_dir = "".to_string();
+    let worker_pool_threads = 1;
+
+    build_router(model_dir, worker_pool_threads).unwrap()
+}
