8b18599ec0a4fe8435a5512d2a088c0c3c730697
gagansingh894 <gds31.gagandeepsingh@gmail.com>
2024-06-15 16:31:28 +0100

    feat: update model store layer implementation

--- jams-core/src/model_store/storage.rs
+++ jams-core/src/model_store/storage.rs
@@ -1,17 +1,17 @@
 use crate::model;
 use crate::model::frameworks::{ModelFramework, CATBOOST, LIGHTGBM, PYTORCH, TENSORFLOW, TORCH};
 use crate::model::predictor::Predictor;
+use chrono::Utc;
 use dashmap::mapref::one::Ref;
+use dashmap::DashMap;
 use serde::Serialize;
+use std::fs;
 use std::sync::Arc;
 
 pub type ModelName = String;
 
 /// Trait representing a storage system for machine learning models.
 pub trait Storage: Send + Sync + 'static {
-    /// Fetches all available models from the storage.
-    fn fetch_models(&self) -> anyhow::Result<()>;
-
     /// Adds a specific machine learning/deep learning model
     fn add_model(&self, model_name: ModelName, model_path: &str) -> anyhow::Result<()>;
 
@@ -99,6 +99,185 @@ impl Model {
 
         Model { predictor, info }
     }
+}
+
+/// Loads models from the specified directory and returns a DashMap containing the models.
+///
+/// The models have a specific name format which allows us to identify the model framework
+/// `<model_framework>-<model_name>`
+/// The `model_framework` and `model_name` are separated by `-`
+///
+/// This function reads the contents of the provided directory, identifies model files based on
+/// their filenames, and loads them into a `DashMap` where the keys are model names and the values
+/// are `Arc`-wrapped `Model` instances.
+///
+/// # Arguments
+///
+/// * `model_dir` - A `String` specifying the path to the directory containing model files.
+///
+/// # Returns
+///
+/// A `Result` containing a `DashMap` with model names as keys and `Arc<Model>` as values.
+/// On failure, returns an `anyhow::Error` with details about the error.
+///
+/// # Errors
+///
+/// This function will return an error if it fails to read the directory, convert file paths, or
+/// load any of the models.
+pub fn load_models(model_dir: String) -> anyhow::Result<DashMap<ModelName, Arc<Model>>> {
+    let models: DashMap<ModelName, Arc<Model>> = DashMap::new();
+
+    let dir = match fs::read_dir(model_dir) {
+        Ok(dir) => dir,
+        Err(e) => {
+            anyhow::bail!("Failed to read dir: {}", e)
+        }
+    };
+
+    for file in dir {
+        let file = match file {
+            Ok(file) => file,
+            Err(e) => {
+                anyhow::bail!("Failed to read file: {}", e)
+            }
+        };
+        let (file_path, file_name) = (file.path(), file.file_name());
+        let full_path = match file_path.into_os_string().into_string() {
+            Ok(full_path) => full_path,
+            Err(_) => {
+                anyhow::bail!("Failed to convert OsString to String")
+            }
+        };
+        let file_name = match file_name.to_str() {
+            None => {
+                anyhow::bail!("Failed to convert OsString to str")
+            }
+            Some(file_name) => file_name,
+        };
+
+        if file_name.contains(TENSORFLOW) {
+            let prefix = format!("{}-", TENSORFLOW);
+            match file_name.to_string().strip_prefix(&prefix) {
+                None => {
+                    anyhow::bail!(
+                        "Failed to strip prefix {} from file name {}",
+                        prefix,
+                        file_name
+                    )
+                }
+                Some(model_name) => {
+                    let predictor = model::tensorflow::Tensorflow::load(full_path.as_str())?;
+                    let now = Utc::now();
+                    let model = Model::new(
+                        Arc::new(predictor),
+                        model_name.to_string(),
+                        TENSORFLOW,
+                        full_path.clone(),
+                        now.to_rfc3339(),
+                    );
+                    models.insert(model_name.to_string(), Arc::new(model));
+                    log::info!("Successfully loaded model from path: {} ✅", full_path);
+                }
+            }
+        } else if file_name.contains(TORCH) {
+            // Torch and PyTorch are same models. PyTorch is a python wrapper around Torch
+            let prefix = format!("{}-", TORCH);
+            match file_name.to_string().strip_prefix(&prefix) {
+                None => {
+                    let prefix = format!("{}-", PYTORCH);
+                    match file_name.to_string().strip_prefix(&prefix) {
+                        None => {
+                            anyhow::bail!(
+                                "Failed to strip prefix {} from file name {}",
+                                prefix,
+                                file_name
+                            )
+                        }
+                        Some(model_name) => {
+                            let predictor = model::torch::Torch::load(full_path.as_str())?;
+                            let now = Utc::now();
+                            let model = Model::new(
+                                Arc::new(predictor),
+                                model_name.to_string(),
+                                PYTORCH, // TORCH can also be used, but they are aliases
+                                full_path.clone(),
+                                now.to_rfc3339(),
+                            );
+                            models.insert(model_name.to_string(), Arc::new(model));
+                            log::info!("Successfully loaded model from path: {} ✅", full_path);
+                        }
+                    }
+                }
+                Some(model_name) => {
+                    let predictor = model::torch::Torch::load(full_path.as_str())?;
+                    let now = Utc::now();
+                    let model = Model::new(
+                        Arc::new(predictor),
+                        model_name.to_string(),
+                        PYTORCH, // TORCH can also be used, but they are aliases
+                        full_path.clone(),
+                        now.to_rfc2822(),
+                    );
+                    models.insert(model_name.to_string(), Arc::new(model));
+                    log::info!("Successfully loaded model from path: {} ✅", full_path);
+                }
+            }
+        } else if file_name.contains(CATBOOST) {
+            let prefix = format!("{}-", CATBOOST);
+            match file_name.to_string().strip_prefix(&prefix) {
+                None => {
+                    anyhow::bail!(
+                        "Failed to strip prefix {} from file name {}",
+                        prefix,
+                        file_name
+                    )
+                }
+                Some(model_name) => {
+                    let predictor = model::catboost::Catboost::load(full_path.as_str())?;
+                    let now = Utc::now();
+                    let model = Model::new(
+                        Arc::new(predictor),
+                        model_name.to_string(),
+                        CATBOOST,
+                        full_path.clone(),
+                        now.to_rfc2822(),
+                    );
+                    models.insert(model_name.to_string(), Arc::new(model));
+                    log::info!("Successfully loaded model from path: {} ✅", full_path);
+                }
+            }
+        } else if file_name.contains(LIGHTGBM) {
+            let prefix = format!("{}-", LIGHTGBM);
+            match file_name.to_string().strip_prefix(&prefix) {
+                None => {
+                    anyhow::bail!(
+                        "Failed to strip prefix {} from file name {}",
+                        prefix,
+                        file_name
+                    )
+                }
+                Some(model_name) => {
+                    let predictor = model::lightgbm::LightGBM::load(full_path.as_str())?;
+                    let now = Utc::now();
+                    let model = Model::new(
+                        Arc::new(predictor),
+                        model_name.to_string(),
+                        LIGHTGBM,
+                        full_path.clone(),
+                        now.to_rfc2822(),
+                    );
+                    models.insert(model_name.to_string(), Arc::new(model));
+                    log::info!("Successfully loaded model from path: {} ✅", full_path);
+                }
+            }
+        } else {
+            log::warn!(
+                "Unexpected model framework encountered in file ⚠️. \n File: {} \n",
+                file_name
+            );
+        }
+    }
+    Ok(models)
 }
 
 /// Loads a machine learning model based on the specified framework and model path.
