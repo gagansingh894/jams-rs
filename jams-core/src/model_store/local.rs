ddcf7ca658c907f25b6f11f21e26ffada3c30655
gagansingh894 <gds31.gagandeepsingh@gmail.com>
2024-06-15 16:33:11 +0100

    refactor: fetch models logic

--- jams-core/src/model_store/local.rs
+++ jams-core/src/model_store/local.rs
@@ -1,12 +1,9 @@
-use crate::model;
-use crate::model::frameworks::{CATBOOST, LIGHTGBM, PYTORCH, TENSORFLOW, TORCH};
 use crate::model_store::storage::{
-    extract_framework_from_path, load_predictor, Metadata, Model, ModelName, Storage,
+    extract_framework_from_path, load_models, load_predictor, Metadata, Model, ModelName, Storage,
 };
 use chrono::Utc;
 use dashmap::mapref::one::Ref;
 use dashmap::DashMap;
-use std::fs;
 use std::sync::Arc;
 
 /// A local model store that manages models stored in a specified directory.
@@ -36,211 +33,28 @@ impl LocalModelStore {
     /// - `Err(anyhow::Error)`: If there was an error creating the instance.
     ///
     pub fn new(model_dir: String) -> anyhow::Result<Self> {
-        let models: DashMap<ModelName, Arc<Model>> = DashMap::new();
-        Ok(LocalModelStore { models, model_dir })
-    }
-
-    /// Loads all models from the specified directory.
-    ///
-    /// The models have a specific name format which allows us to identify the model framework
-    /// `<model_framework>-<model_name>`
-    /// The `model_framework` and `model_name` are separated by `-`
-    ///
-    /// This method iterates through the models in the directory, identifies the model framework based on the file name,
-    /// and loads each model into the `models` map.
-    ///
-    /// # Returns
-    /// - `Ok(())`: If all models were successfully fetched and loaded.
-    /// - `Err(anyhow::Error)`: If there was an error fetching or loading the models.
-    ///
-    fn load_models(&self) -> anyhow::Result<()> {
-        let dir = match fs::read_dir(&self.model_dir) {
-            Ok(dir) => dir,
-            Err(e) => {
-                anyhow::bail!("Failed to read dir: {}", e)
-            }
-        };
-
-        for file in dir {
-            let file = match file {
-                Ok(file) => file,
-                Err(e) => {
-                    anyhow::bail!("Failed to read file: {}", e)
-                }
-            };
-            let (file_path, file_name) = (file.path(), file.file_name());
-            let full_path = match file_path.into_os_string().into_string() {
-                Ok(full_path) => full_path,
-                Err(_) => {
-                    anyhow::bail!("Failed to convert OsString to String")
-                }
-            };
-            let file_name = match file_name.to_str() {
-                None => {
-                    anyhow::bail!("Failed to convert OsString to str")
-                }
-                Some(file_name) => file_name,
-            };
-
-            if file_name.contains(TENSORFLOW) {
-                let prefix = format!("{}-", TENSORFLOW);
-                match file_name.to_string().strip_prefix(&prefix) {
-                    None => {
-                        anyhow::bail!(
-                            "Failed to strip prefix {} from file name {}",
-                            prefix,
-                            file_name
-                        )
-                    }
-                    Some(model_name) => {
-                        let predictor = model::tensorflow::Tensorflow::load(full_path.as_str())?;
-                        let now = Utc::now();
-                        let model = Model::new(
-                            Arc::new(predictor),
-                            model_name.to_string(),
-                            TENSORFLOW,
-                            full_path.clone(),
-                            now.to_rfc3339(),
-                        );
-                        self.models.insert(model_name.to_string(), Arc::new(model));
-                        log::info!("Successfully loaded model from path: {} ✅", full_path);
-                    }
-                }
-            } else if file_name.contains(TORCH) {
-                // Torch and PyTorch are same models. PyTorch is a python wrapper around Torch
-                let prefix = format!("{}-", TORCH);
-                match file_name.to_string().strip_prefix(&prefix) {
-                    None => {
-                        let prefix = format!("{}-", PYTORCH);
-                        match file_name.to_string().strip_prefix(&prefix) {
-                            None => {
-                                anyhow::bail!(
-                                    "Failed to strip prefix {} from file name {}",
-                                    prefix,
-                                    file_name
-                                )
-                            }
-                            Some(model_name) => {
-                                let predictor = model::torch::Torch::load(full_path.as_str())?;
-                                let now = Utc::now();
-                                let model = Model::new(
-                                    Arc::new(predictor),
-                                    model_name.to_string(),
-                                    PYTORCH, // TORCH can also be used, but they are aliases
-                                    full_path.clone(),
-                                    now.to_rfc3339(),
-                                );
-                                self.models.insert(model_name.to_string(), Arc::new(model));
-                                log::info!("Successfully loaded model from path: {} ✅", full_path);
-                            }
-                        }
-                    }
-                    Some(model_name) => {
-                        let predictor = model::torch::Torch::load(full_path.as_str())?;
-                        let now = Utc::now();
-                        let model = Model::new(
-                            Arc::new(predictor),
-                            model_name.to_string(),
-                            PYTORCH, // TORCH can also be used, but they are aliases
-                            full_path.clone(),
-                            now.to_rfc2822(),
-                        );
-                        self.models.insert(model_name.to_string(), Arc::new(model));
-                        log::info!("Successfully loaded model from path: {} ✅", full_path);
-                    }
-                }
-            } else if file_name.contains(CATBOOST) {
-                let prefix = format!("{}-", CATBOOST);
-                match file_name.to_string().strip_prefix(&prefix) {
-                    None => {
-                        anyhow::bail!(
-                            "Failed to strip prefix {} from file name {}",
-                            prefix,
-                            file_name
-                        )
-                    }
-                    Some(model_name) => {
-                        let predictor = model::catboost::Catboost::load(full_path.as_str())?;
-                        let now = Utc::now();
-                        let model = Model::new(
-                            Arc::new(predictor),
-                            model_name.to_string(),
-                            CATBOOST,
-                            full_path.clone(),
-                            now.to_rfc2822(),
-                        );
-                        self.models.insert(model_name.to_string(), Arc::new(model));
-                        log::info!("Successfully loaded model from path: {} ✅", full_path);
-                    }
-                }
-            } else if file_name.contains(LIGHTGBM) {
-                let prefix = format!("{}-", LIGHTGBM);
-                match file_name.to_string().strip_prefix(&prefix) {
-                    None => {
-                        anyhow::bail!(
-                            "Failed to strip prefix {} from file name {}",
-                            prefix,
-                            file_name
-                        )
-                    }
-                    Some(model_name) => {
-                        let predictor = model::lightgbm::LightGBM::load(full_path.as_str())?;
-                        let now = Utc::now();
-                        let model = Model::new(
-                            Arc::new(predictor),
-                            model_name.to_string(),
-                            LIGHTGBM,
-                            full_path.clone(),
-                            now.to_rfc2822(),
-                        );
-                        self.models.insert(model_name.to_string(), Arc::new(model));
-                        log::info!("Successfully loaded model from path: {} ✅", full_path);
-                    }
-                }
-            } else {
-                log::warn!(
-                    "Unexpected model framework encountered in file ⚠️. \n File: {} \n",
-                    file_name
-                );
-            }
-        }
-        Ok(())
-    }
-}
-
-impl Storage for LocalModelStore {
-    /// Fetches and loads all models from the specified directory.
-    ///
-    /// The models have a specific name format which allows us to identify the model framework
-    /// `<model_framework>-<model_name>`
-    /// The `model_framework` and `model_name` are separated by `-`
-    ///
-    /// This method iterates through the models in the directory, identifies the model framework based on the file name,
-    /// and loads each model into the `models` map.
-    ///
-    /// # Returns
-    /// - `Ok(())`: If all models were successfully fetched and loaded.
-    /// - `Err(anyhow::Error)`: If there was an error fetching or loading the models.
-    ///
-    fn fetch_models(&self) -> anyhow::Result<()> {
-        match self.model_dir.is_empty() {
+        let models = match model_dir.is_empty() {
             true => {
                 log::warn!("No model directory specified, hence no models will be loaded ⚠️");
-                Ok(())
+                let models: DashMap<ModelName, Arc<Model>> = DashMap::new();
+                models
             }
-            false => match self.load_models() {
-                Ok(_) => {
+            false => match load_models(model_dir.clone()) {
+                Ok(models) => {
                     log::info!("Successfully fetched valid models from directory ✅");
-                    Ok(())
+                    models
                 }
                 Err(e) => {
                     anyhow::bail!("Failed to fetch models - {}", e.to_string());
                 }
             },
-        }
-    }
+        };
 
-    ///
+        Ok(LocalModelStore { models, model_dir })
+    }
+}
+
+impl Storage for LocalModelStore {
     /// This function attempts to extract the framework from the given model path,
     /// load the model using the identified framework, and then store the model
     /// in the model store along with its metadata.
@@ -357,12 +171,12 @@ impl Storage for LocalModelStore {
     /// This function returns an `anyhow::Result` containing a vector of `Metadata`.
     ///
     fn get_models(&self) -> anyhow::Result<Vec<Metadata>> {
-        let model_names: Vec<Metadata> = self
+        let model: Vec<Metadata> = self
             .models
             .iter()
             .map(|f| f.value().info.to_owned())
             .collect();
-        Ok(model_names)
+        Ok(model)
     }
 
     /// Deletes a model from the model store.
@@ -396,55 +210,48 @@ mod tests {
     #[test]
     fn successfully_load_models_from_different_frameworks_into_local_model_store() {
         let model_dir = "tests/model_storage/local_model_store";
-        let local_model_store = LocalModelStore::new(model_dir.to_string()).unwrap();
 
-        // load models
-        let result = local_model_store.fetch_models();
+        // initialize and load models
+        let local_model_store = LocalModelStore::new(model_dir.to_string());
 
         // assert
-        assert!(result.is_ok());
-        assert_ne!(local_model_store.models.len(), 0);
+        assert!(local_model_store.is_ok());
+        assert_ne!(local_model_store.unwrap().models.len(), 0);
     }
 
     #[test]
     fn successfully_get_model_from_local_model_store() {
         let model_dir = "tests/model_storage/local_model_store";
-        let local_model_store = LocalModelStore::new(model_dir.to_string()).unwrap();
 
         // load models
-        let result = local_model_store.fetch_models();
+        let local_model_store = LocalModelStore::new(model_dir.to_string()).unwrap();
         let model = local_model_store.get_model("my_awesome_autompg_model".to_string());
 
         // assert
-        assert!(result.is_ok());
         assert!(model.is_some());
     }
 
     #[test]
     fn fails_to_get_model_from_local_model_store_when_model_name_is_wrong() {
         let model_dir = "tests/model_storage/local_model_store";
-        let local_model_store = LocalModelStore::new(model_dir.to_string()).unwrap();
 
         // load models
-        let result = local_model_store.fetch_models();
+        let local_model_store = LocalModelStore::new(model_dir.to_string()).unwrap();
         let model = local_model_store.get_model("model_which_does_not_exist".to_string());
 
         // assert
-        assert!(result.is_ok());
         assert!(model.is_none());
     }
 
     #[test]
     fn successfully_get_models_from_local_model_store() {
         let model_dir = "tests/model_storage/local_model_store";
-        let local_model_store = LocalModelStore::new(model_dir.to_string()).unwrap();
 
         // load models
-        let result = local_model_store.fetch_models();
+        let local_model_store = LocalModelStore::new(model_dir.to_string()).unwrap();
         let models = local_model_store.get_models();
 
         // assert
-        assert!(result.is_ok());
         assert!(models.is_ok());
         assert_ne!(models.unwrap().len(), 0);
     }
@@ -452,40 +259,34 @@ mod tests {
     #[test]
     fn successfully_deletes_model_in_the_local_model_store() {
         let model_dir = "tests/model_storage/local_model_store";
-        let local_model_store = LocalModelStore::new(model_dir.to_string()).unwrap();
 
         // load models
-        let result = local_model_store.fetch_models();
+        let local_model_store = LocalModelStore::new(model_dir.to_string()).unwrap();
         let deletion = local_model_store.delete_model("my_awesome_autompg_model".to_string());
 
         // assert
-        assert!(result.is_ok());
         assert!(deletion.is_ok());
     }
 
     #[test]
     fn fails_to_deletes_model_in_the_local_model_store() {
         let model_dir = "tests/model_storage/local_model_store";
-        let local_model_store = LocalModelStore::new(model_dir.to_string()).unwrap();
 
         // load models
-        let result = local_model_store.fetch_models();
+        let local_model_store = LocalModelStore::new(model_dir.to_string()).unwrap();
         let deletion = local_model_store.delete_model("model_which_does_not_exist".to_string());
 
         // assert
-        assert!(result.is_ok());
         assert!(deletion.is_err());
     }
 
     #[test]
     fn successfully_update_model_in_the_local_model_store() {
         let model_dir = "tests/model_storage/local_model_store";
-        let local_model_store = LocalModelStore::new(model_dir.to_string()).unwrap();
         let model_name = "my_awesome_autompg_model".to_string();
 
         // load models
-        let result = local_model_store.fetch_models();
-        assert!(result.is_ok());
+        let local_model_store = LocalModelStore::new(model_dir.to_string()).unwrap();
 
         // retrieve timestamp from existing to model for assertion
         let model = local_model_store
@@ -510,12 +311,10 @@ mod tests {
     #[test]
     fn fails_to_update_model_in_the_local_model_store_when_model_name_is_incorrect() {
         let model_dir = "tests/model_storage/local_model_store";
-        let local_model_store = LocalModelStore::new(model_dir.to_string()).unwrap();
         let incorrect_model_name = "my_awesome_autompg_model_incorrect".to_string();
 
         // load models
-        let result = local_model_store.fetch_models();
-        assert!(result.is_ok());
+        let local_model_store = LocalModelStore::new(model_dir.to_string()).unwrap();
 
         // update model with incorrect model name
         let update = local_model_store.update_model(incorrect_model_name);
@@ -527,11 +326,10 @@ mod tests {
     #[test]
     fn successfully_add_model_in_the_local_model_store() {
         let model_dir = "tests/model_storage/local_model_store";
-        let local_model_store = LocalModelStore::new(model_dir.to_string()).unwrap();
 
         // load models
-        let result = local_model_store.fetch_models();
-        assert!(result.is_ok());
+        let local_model_store = LocalModelStore::new(model_dir.to_string()).unwrap();
+
         // delete model to set up test
         local_model_store
             .delete_model("my_awesome_penguin_model".to_string())
@@ -558,11 +356,9 @@ mod tests {
     #[test]
     fn fails_to_add_model_in_the_local_model_store_when_the_model_path_is_wrong() {
         let model_dir = "tests/model_storage/local_model_store";
-        let local_model_store = LocalModelStore::new(model_dir.to_string()).unwrap();
 
         // load models
-        let result = local_model_store.fetch_models();
-        assert!(result.is_ok());
+        let local_model_store = LocalModelStore::new(model_dir.to_string()).unwrap();
 
         // add model
         let add = local_model_store.add_model(
