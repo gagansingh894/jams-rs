3fd51af15dd60d27b8abf3c85bb776e1fa03a5f2
gagansingh894 <gds31.gagandeepsingh@gmail.com>
2024-06-20 23:47:17 +0100

    fix: parity with cloud model store

--- jams-core/src/model_store/local.rs
+++ jams-core/src/model_store/local.rs
@@ -1,3 +1,6 @@
+use crate::model_store::common::{
+    cleanup, unpack_tarball, DOWNLOADED_MODELS_DIRECTORY_NAME_PREFIX,
+};
 use crate::model_store::storage::{
     extract_framework_from_path, load_models, load_predictor, Metadata, Model, ModelName, Storage,
 };
@@ -5,7 +8,9 @@ use async_trait::async_trait;
 use chrono::Utc;
 use dashmap::mapref::one::Ref;
 use dashmap::DashMap;
+use std::fs;
 use std::sync::Arc;
+use uuid::Uuid;
 
 /// A local model store that manages models stored in a specified directory.
 ///
@@ -17,8 +22,12 @@ use std::sync::Arc;
 /// - `models` (DashMap<ModelName, Arc&ltModel&gt>): A thread-safe map of model names to their respective Model struct instances.
 /// - `model_dir` (String): The directory where models are stored.
 pub struct LocalModelStore {
+    /// A thread-safe map of model names to their corresponding models.
     pub models: Arc<DashMap<ModelName, Arc<Model>>>,
-    pub model_dir: String,
+    /// Directory on the file system containing models in .tar.gz format
+    pub local_model_store_dir: String,
+    /// Temporary directory in which models are stored and read from. This is cleaned up when object is dropped
+    temp_model_dir: String,
 }
 
 impl LocalModelStore {
@@ -33,14 +42,38 @@ impl LocalModelStore {
     /// - `Ok(LocalModelStore)`: If the instance was successfully created.
     /// - `Err(anyhow::Error)`: If there was an error creating the instance.
     ///
-    pub async fn new(model_dir: String) -> anyhow::Result<Self> {
-        let models = match model_dir.is_empty() {
+    pub async fn new(local_model_store_dir: String) -> anyhow::Result<Self> {
+        // Directory, which stores the unpacked
+        let temp_model_dir = format!(
+            "{}/{}_{}",
+            std::env::var("HOME").unwrap_or("/usr/local".to_string()),
+            DOWNLOADED_MODELS_DIRECTORY_NAME_PREFIX,
+            Uuid::new_v4(),
+        );
+
+        // unpack
+        match fs::read_dir(local_model_store_dir.as_str()) {
+            Ok(dir) => {
+                for entry in dir {
+                    let entry = entry?;
+                    let path = entry.path();
+                    unpack_tarball(path.to_str().unwrap(), temp_model_dir.as_str())?
+                }
+            }
+            Err(e) => {
+                anyhow::bail!("Failed to read directory: {}", e)
+            }
+        }
+
+        let models = match local_model_store_dir.is_empty() {
             true => {
-                log::warn!("No model directory specified, hence no models will be loaded ⚠️");
+                log::warn!(
+                    "No local model store directory specified, hence no models will be loaded ⚠️"
+                );
                 let models: DashMap<ModelName, Arc<Model>> = DashMap::new();
                 models
             }
-            false => match load_models(model_dir.clone()).await {
+            false => match load_models(temp_model_dir.clone()).await {
                 Ok(models) => {
                     log::info!("Successfully fetched valid models from directory ✅");
                     models
@@ -53,8 +86,25 @@ impl LocalModelStore {
 
         Ok(LocalModelStore {
             models: Arc::new(models),
-            model_dir,
+            local_model_store_dir,
+            temp_model_dir,
         })
+    }
+}
+
+/// Implements the `Drop` trait for `LocalModelStore`.
+///
+/// This implementation ensures that the temporary directory used for the model store is cleaned up
+/// when the `LocalModelStore` instance is dropped. This helps to avoid leaving temporary files on disk
+/// and ensures proper resource cleanup.
+///
+/// # Fields
+///
+/// * `model_store_dir` - The directory path for the temporary local model store which contains models downloaded from S3.
+///
+impl Drop for LocalModelStore {
+    fn drop(&mut self) {
+        cleanup(self.temp_model_dir.clone())
     }
 }
 
@@ -75,12 +125,19 @@ impl Storage for LocalModelStore {
     /// * The framework cannot be extracted from the model path.
     /// * The model fails to load.
     ///
-    async fn add_model(&self, model_name: ModelName, model_path: &str) -> anyhow::Result<()> {
-        let model_name = model_name.clone();
-        let model_path = model_path.to_string();
-        let models = Arc::clone(&self.models);
+    async fn add_model(&self, model_name: ModelName, _model_path: &str) -> anyhow::Result<()> {
+        let lms_model_name = format!("{}.tar.gz", model_name.clone());
 
-        match extract_framework_from_path(model_path.to_string()) {
+        let local_model_store_path = format!("{}/{}", self.local_model_store_dir, lms_model_name);
+
+        unpack_tarball(
+            local_model_store_path.as_str(),
+            self.temp_model_dir.as_str(),
+        )?;
+
+        let model_path = format!("{}/{}", self.temp_model_dir, model_name);
+
+        match extract_framework_from_path(model_path.clone()) {
             None => {
                 anyhow::bail!("Failed to extract framework from path");
             }
@@ -94,7 +151,7 @@ impl Storage for LocalModelStore {
                         model_path.to_string(),
                         now.to_rfc2822(),
                     );
-                    models.insert(model_name, Arc::new(model));
+                    self.models.insert(model_name, Arc::new(model));
                     Ok(())
                 }
                 Err(e) => {
@@ -132,21 +189,36 @@ impl Storage for LocalModelStore {
             Some(model) => {
                 let (model_framework, model_path) =
                     (model.1.info.framework, model.1.info.path.as_str());
-                match load_predictor(model_framework, model_path).await {
-                    Ok(predictor) => {
-                        let now = Utc::now();
-                        let model = Model::new(
-                            predictor,
-                            model_name.clone(),
-                            model_framework,
-                            model_path.to_string(),
-                            now.to_rfc2822(),
-                        );
-                        self.models.insert(model_name.clone(), Arc::new(model));
-                        Ok(())
-                    }
+
+                // Prepare the actual name from model_name
+                let model = format!("{}-{}.tar.gz", model_framework, model_name);
+
+                // unpack
+                let model_tar_path = format!("{}/{}", self.local_model_store_dir, model);
+                match unpack_tarball(model_tar_path.as_str(), self.temp_model_dir.as_str()) {
+                    Ok(_) => match load_predictor(model_framework, model_path).await {
+                        Ok(predictor) => {
+                            let now = Utc::now();
+                            let model = Model::new(
+                                predictor,
+                                model_name.clone(),
+                                model_framework,
+                                model_path.to_string(),
+                                now.to_rfc2822(),
+                            );
+                            self.models.insert(model_name.clone(), Arc::new(model));
+                            Ok(())
+                        }
+                        Err(e) => {
+                            anyhow::bail!(
+                                "Failed to update the specified model {}: {}",
+                                model_name,
+                                e
+                            )
+                        }
+                    },
                     Err(e) => {
-                        anyhow::bail!("Failed to update the specified model {}: {}", model_name, e)
+                        anyhow::bail!("Failed to update model ❌: {}", e);
                     }
                 }
             }
@@ -215,10 +287,11 @@ impl Storage for LocalModelStore {
 #[cfg(test)]
 mod tests {
     use super::*;
+    use std::time::Duration;
 
     #[tokio::test]
     async fn successfully_load_models_from_different_frameworks_into_local_model_store() {
-        let model_dir = "tests/model_storage/local_model_store";
+        let model_dir = "tests/model_storage/model_store";
 
         // initialize and load models
         let local_model_store = LocalModelStore::new(model_dir.to_string()).await;
@@ -230,11 +303,11 @@ mod tests {
 
     #[tokio::test]
     async fn successfully_get_model_from_local_model_store() {
-        let model_dir = "tests/model_storage/local_model_store";
+        let model_dir = "tests/model_storage/model_store";
 
         // load models
         let local_model_store = LocalModelStore::new(model_dir.to_string()).await.unwrap();
-        let model = local_model_store.get_model("my_awesome_autompg_model".to_string());
+        let model = local_model_store.get_model("my_awesome_reg_model".to_string());
 
         // assert
         assert!(model.is_some());
@@ -242,7 +315,7 @@ mod tests {
 
     #[tokio::test]
     async fn fails_to_get_model_from_local_model_store_when_model_name_is_wrong() {
-        let model_dir = "tests/model_storage/local_model_store";
+        let model_dir = "tests/model_storage/model_store";
 
         // load models
         let local_model_store = LocalModelStore::new(model_dir.to_string()).await.unwrap();
@@ -254,7 +327,7 @@ mod tests {
 
     #[tokio::test]
     async fn successfully_get_models_from_local_model_store() {
-        let model_dir = "tests/model_storage/local_model_store";
+        let model_dir = "tests/model_storage/model_store";
 
         // load models
         let local_model_store = LocalModelStore::new(model_dir.to_string()).await.unwrap();
@@ -267,11 +340,11 @@ mod tests {
 
     #[tokio::test]
     async fn successfully_deletes_model_in_the_local_model_store() {
-        let model_dir = "tests/model_storage/local_model_store";
+        let model_dir = "tests/model_storage/model_store";
 
         // load models
         let local_model_store = LocalModelStore::new(model_dir.to_string()).await.unwrap();
-        let deletion = local_model_store.delete_model("my_awesome_autompg_model".to_string());
+        let deletion = local_model_store.delete_model("my_awesome_reg_model".to_string());
 
         // assert
         assert!(deletion.is_ok());
@@ -279,7 +352,7 @@ mod tests {
 
     #[tokio::test]
     async fn fails_to_deletes_model_in_the_local_model_store() {
-        let model_dir = "tests/model_storage/local_model_store";
+        let model_dir = "tests/model_storage/model_store";
 
         // load models
         let local_model_store = LocalModelStore::new(model_dir.to_string()).await.unwrap();
@@ -291,11 +364,12 @@ mod tests {
 
     #[tokio::test]
     async fn successfully_update_model_in_the_local_model_store() {
-        let model_dir = "tests/model_storage/local_model_store";
-        let model_name = "my_awesome_autompg_model".to_string();
+        let model_dir = "tests/model_storage/model_store";
+        let model_name = "my_awesome_reg_model".to_string();
 
         // load models
         let local_model_store = LocalModelStore::new(model_dir.to_string()).await.unwrap();
+        tokio::time::sleep(Duration::from_secs_f32(0.95)).await;
 
         // retrieve timestamp from existing to model for assertion
         let model = local_model_store
@@ -305,6 +379,7 @@ mod tests {
 
         // update model
         let update = local_model_store.update_model(model_name.clone()).await;
+
         assert!(update.is_ok());
         let updated_model = local_model_store
             .get_model(model_name.clone())
@@ -319,7 +394,7 @@ mod tests {
 
     #[tokio::test]
     async fn fails_to_update_model_in_the_local_model_store_when_model_name_is_incorrect() {
-        let model_dir = "tests/model_storage/local_model_store";
+        let model_dir = "tests/model_storage/model_store";
         let incorrect_model_name = "my_awesome_autompg_model_incorrect".to_string();
 
         // load models
@@ -334,7 +409,7 @@ mod tests {
 
     #[tokio::test]
     async fn successfully_add_model_in_the_local_model_store() {
-        let model_dir = "tests/model_storage/local_model_store";
+        let model_dir = "tests/model_storage/model_store";
 
         // load models
         let local_model_store = LocalModelStore::new(model_dir.to_string()).await.unwrap();
@@ -350,23 +425,20 @@ mod tests {
 
         // add model
         let add = local_model_store
-            .add_model(
-                "my_awesome_penguin_model".to_string(),
-                "tests/model_storage/local_model_store/tensorflow-my_awesome_penguin_model",
-            )
+            .add_model("tensorflow-my_awesome_penguin_model".to_string(), "")
             .await;
         let num_models_after_add = local_model_store.get_models().unwrap().len();
 
         // assert
         assert!(add.is_ok());
-        let model = local_model_store.get_model("my_awesome_penguin_model".to_string());
+        let model = local_model_store.get_model("tensorflow-my_awesome_penguin_model".to_string());
         assert!(model.is_some());
         assert_eq!(num_models_after_add - num_models, 1);
     }
 
     #[tokio::test]
     async fn fails_to_add_model_in_the_local_model_store_when_the_model_path_is_wrong() {
-        let model_dir = "tests/model_storage/local_model_store";
+        let model_dir = "tests/model_storage/model_store";
 
         // load models
         let local_model_store = LocalModelStore::new(model_dir.to_string()).await.unwrap();
@@ -375,7 +447,7 @@ mod tests {
         let add = local_model_store
             .add_model(
                 "my_awesome_penguin_model".to_string(),
-                "tests/model_storage/local_model_store/model_which_does_not_exist",
+                "tests/model_storage/model_store/model_which_does_not_exist",
             )
             .await;
 
