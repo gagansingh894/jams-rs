89692746cc7a58a46465cf8dc256ff7523e4bef3
gagansingh894 <gds31.gagandeepsingh@gmail.com>
2024-10-16 19:30:38 +0100

    feat: add helper functions

---
+++ clients/python/jams/tests/helper.py
@@ -0,0 +1,17 @@
+import os
+
+
+def get_http_url() -> str:
+    hostname = os.environ.get('JAMS_HTTP_HOSTNAME')
+    if hostname is None:
+        hostname = '0.0.0.0'
+
+    return f'{hostname}:3000'
+
+
+def get_grpc_url() -> str:
+    hostname = os.environ.get('JAMS_GRPC_HOSTNAME')
+    if hostname is None:
+        hostname = '0.0.0.0'
+
+    return f'{hostname}:4000'
