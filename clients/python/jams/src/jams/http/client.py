f19818a0153bad4301952852611e6f7ac4ec312e
gagansingh894 <gds31.gagandeepsingh@gmail.com>
2024-10-16 19:30:00 +0100

    fix: use non deprecated methods

--- clients/python/jams/src/jams/http/client.py
+++ clients/python/jams/src/jams/http/client.py
@@ -19,14 +19,14 @@ class HttpClient:
 
     def predict(self, model_name: str, model_input: str) -> type.Prediction:
         url = f"{self.base_url}/api/predict"
-        request = models.PredictRequest(model_name=model_name, input=model_input).dict()
+        request = models.PredictRequest(model_name=model_name, input=model_input).model_dump()
 
         try:
             response = requests.post(url, json=request)
             if response.status_code != 200:
                 raise Exception(f"predict failed with {response.status_code}")
             try:
-                resp_obj = models.PredictResponse.parse_obj(response.json())
+                resp_obj = models.PredictResponse.model_validate(response.json())
                 return type.Prediction(resp_obj.output)
             except ValueError as e:
                 raise Exception(f"fail to parse predict response: {e}")
@@ -35,7 +35,7 @@ class HttpClient:
 
     def add_model(self, model_name: str) -> None:
         url = f"{self.base_url}/api/models"
-        request = models.AddModelsRequest(model_name=model_name).dict()
+        request = models.AddModelsRequest(model_name=model_name).model_dump()
         try:
             response = requests.post(url, json=request)
             if response.status_code != 200:
@@ -45,7 +45,7 @@ class HttpClient:
 
     def update_model(self, model_name: str) -> None:
         url = f"{self.base_url}/api/models"
-        request = models.UpdateModelsRequest(model_name=model_name).dict()
+        request = models.UpdateModelsRequest(model_name=model_name).model_dump()
         try:
             response = requests.put(url, json=request)
             if response.status_code != 200:
@@ -71,6 +71,6 @@ class HttpClient:
         if response.status_code != 200:
             raise Exception(f"get model failed with {response.status_code}")
         try:
-            return models.GetModelsResponse.parse_obj(response.json())
+            return models.GetModelsResponse.model_validate(response.json())
         except ValueError as e:
             raise Exception(f"fail to parse get model response: {e}")
