70981466dfae1c1629924b126b0c1486a5fdaa1b
gagansingh894 <gds31.gagandeepsingh@gmail.com>
2024-10-20 19:15:44 +0100

    feat: parse config files

--- jams/src/cli.rs
+++ jams/src/cli.rs
@@ -1,5 +1,6 @@
 use clap::{Args, Parser, Subcommand};
 use jams_core::model::predictor::Predictor;
+use serde::Deserialize;
 use std::fs;
 
 /// CLI for starting an J.A.M.S
@@ -29,7 +30,10 @@ pub enum Commands {
 #[derive(Parser, Debug)]
 pub struct StartCommands {
     #[clap(subcommand)]
-    pub cmd: StartSubCommands,
+    pub cmd: Option<StartSubCommands>,
+
+    #[arg(short = 'f', long)]
+    pub file: Option<String>,
 }
 
 #[derive(Subcommand, Debug)]
@@ -111,6 +115,25 @@ pub struct PredictCommandArgs {
     /// Path to JSON file containing input data
     #[clap(long)]
     pub input_path: Option<String>,
+}
+
+// Top level struct to hold the TOML data.
+#[derive(Deserialize)]
+pub struct Data {
+    pub config: Config,
+}
+
+// Config struct holds to data from the `[config]` section.
+#[derive(Deserialize)]
+struct Config {
+    pub protocol: String,
+    pub port: Option<u16>,
+    pub store: String,
+    pub num_workers: Option<usize>,
+    pub poll_interval: Option<u64>,
+    pub model_dor: Option<String>,
+    pub azure_storage_container_name: Option<String>,
+    pub s3_bucket_name: Option<String>,
 }
 
 pub fn predict(
--- jams/src/main.rs
+++ jams/src/main.rs
@@ -1,7 +1,14 @@
-use crate::cli::{predict, Commands, PredictSubCommands, StartSubCommands};
+use crate::cli::{predict, Commands, PredictSubCommands, StartSubCommands, Data};
 use clap::Parser;
+use std::fs;
 
 mod cli;
+
+const GRPC: &'static str = "grpc";
+const HTTP: &'static str = "http";
+const LOCAL: &'static str = "local";
+const AZURE: &'static str = "azure";
+const AWS: &'static str = "aws";
 
 #[cfg(not(tarpaulin_include))]
 #[tokio::main]
@@ -29,48 +36,104 @@ J.A.M.S - Just Another Model Server
 
     match cli.cmd {
         Commands::Start(subcommands) => {
-            match subcommands.cmd {
-                StartSubCommands::Http(args) => {
-                    let config = jams_serve::common::server::Config {
-                        model_dir: args.model_dir,
-                        port: args.port,
-                        use_debug_level: args.use_debug_level,
-                        num_workers: args.num_workers,
-                        with_s3_model_store: args.with_s3_model_store,
-                        s3_bucket_name: args.s3_bucket_name,
-                        with_azure_model_store: args.with_azure_model_store,
-                        azure_storage_container_name: args.azure_storage_container_name,
-                        poll_interval: args.poll_interval,
+            match subcommands.file {
+                Some(file_path) => {
+                    let contents = match fs::read_to_string(file_path) {
+                        Ok(c) => c,
+                        Err(e) => {
+                            anyhow::bail!("Failed to read config file ❌: {}", e.to_string())
+                        }
                     };
 
-                    jams_serve::http::server::start(config)
-                        .await
-                        .expect("Failed to start server ❌ \n");
+                    let config_data: Data = match toml::from_str(contents.as_str()) {
+                        Ok(config_data) => config_data,
+                        Err(e) => {
+                            anyhow::bail!("Failed to parse config file ❌: {}", e.to_string())
+                        }
+                    };
 
-                    // shutdown signal received
-                    tracing::error!("Shutdown signal received ⚠️");
-                    Ok(())
+                    let config = jams_serve::common::server::Config {
+                        model_dir: config_data.config.model_dor,
+                        port: config_data.config.port,
+                        use_debug_level: Some(false),
+                        num_workers: config_data.config.num_workers,
+                        with_s3_model_store: Some(config_data.config.store == AWS),
+                        s3_bucket_name: config_data.config.s3_bucket_name,
+                        with_azure_model_store: Some(config_data.config.store == AZURE),
+                        azure_storage_container_name: config_data
+                            .config
+                            .azure_storage_container_name,
+                        poll_interval: config_data.config.poll_interval,
+                    };
+
+                    if config_data.config.protocol == GRPC {
+                        jams_serve::grpc::server::start(config)
+                            .await
+                            .expect("Failed to start server ❌ \n");
+
+                        // shutdown signal received
+                        tracing::error!("Shutdown signal received ⚠️");
+                        Ok(())
+                    } else {
+                        jams_serve::http::server::start(config)
+                            .await
+                            .expect("Failed to start server ❌ \n");
+
+                        // shutdown signal received
+                        tracing::error!("Shutdown signal received ⚠️");
+                        Ok(())
+                    }
                 }
-                StartSubCommands::Grpc(args) => {
-                    let config = jams_serve::common::server::Config {
-                        model_dir: args.model_dir,
-                        port: args.port,
-                        use_debug_level: args.use_debug_level,
-                        num_workers: args.num_workers,
-                        with_s3_model_store: args.with_s3_model_store,
-                        s3_bucket_name: args.s3_bucket_name,
-                        with_azure_model_store: args.with_azure_model_store,
-                        azure_storage_container_name: args.azure_storage_container_name,
-                        poll_interval: args.poll_interval,
-                    };
+                None => {
+                    match subcommands.cmd {
+                        None => {
+                            anyhow::bail!(
+                        "Either pass path to config file using -f or use http/grpc subcommands "
+                    );
+                        }
+                        Some(StartSubCommands::Http(args)) => {
+                            let config = jams_serve::common::server::Config {
+                                model_dir: args.model_dir,
+                                port: args.port,
+                                use_debug_level: args.use_debug_level,
+                                num_workers: args.num_workers,
+                                with_s3_model_store: args.with_s3_model_store,
+                                s3_bucket_name: args.s3_bucket_name,
+                                with_azure_model_store: args.with_azure_model_store,
+                                azure_storage_container_name: args.azure_storage_container_name,
+                                poll_interval: args.poll_interval,
+                            };
 
-                    jams_serve::grpc::server::start(config)
-                        .await
-                        .expect("Failed to start server ❌ \n");
+                            jams_serve::http::server::start(config)
+                                .await
+                                .expect("Failed to start server ❌ \n");
 
-                    // shutdown signal received
-                    tracing::error!("Shutdown signal received ⚠️");
-                    Ok(())
+                            // shutdown signal received
+                            tracing::error!("Shutdown signal received ⚠️");
+                            Ok(())
+                        }
+                        Some(StartSubCommands::Grpc(args)) => {
+                            let config = jams_serve::common::server::Config {
+                                model_dir: args.model_dir,
+                                port: args.port,
+                                use_debug_level: args.use_debug_level,
+                                num_workers: args.num_workers,
+                                with_s3_model_store: args.with_s3_model_store,
+                                s3_bucket_name: args.s3_bucket_name,
+                                with_azure_model_store: args.with_azure_model_store,
+                                azure_storage_container_name: args.azure_storage_container_name,
+                                poll_interval: args.poll_interval,
+                            };
+
+                            jams_serve::grpc::server::start(config)
+                                .await
+                                .expect("Failed to start server ❌ \n");
+
+                            // shutdown signal received
+                            tracing::error!("Shutdown signal received ⚠️");
+                            Ok(())
+                        }
+                    }
                 }
             }
         }
