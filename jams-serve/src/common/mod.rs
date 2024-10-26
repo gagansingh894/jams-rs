45b5a569fc734cc30fc3a1baedf0d69de39d1827
gagansingh894 <gds31.gagandeepsingh@gmail.com>
2024-10-26 19:53:58 +0100

    feat: restructure

--- jams-serve/src/common/mod.rs
+++ jams-serve/src/common/mod.rs
@@ -2,4 +2,5 @@ pub mod server;
 pub mod shutdown;
 pub mod state;
 pub mod worker;
-pub mod opentelemetry;
+mod opentelemetry;
+pub mod instrument;
--- jams-serve/src/common/opentelemetry/mod.rs
+++
@@ -1,6 +0,0 @@
-mod datadog;
-mod signoz;
-pub mod jaeger;
-
-
-pub fn init() {}
