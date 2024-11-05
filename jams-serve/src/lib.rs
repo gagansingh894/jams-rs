ffb9f568d51a554cd3d70e69364cc08033f54348
gagansingh894 <gds31.gagandeepsingh@gmail.com>
2024-11-05 01:25:19 +0000

    feat: allocate worker threads based on physical cpu count

--- jams-serve/src/lib.rs
+++ jams-serve/src/lib.rs
@@ -4,17 +4,37 @@ pub mod http;
 
 use crate::common::server;
 use crate::common::server::HTTP;
-use crate::common::state::build_app_state_from_config;
+use crate::common::state::build_app_state;
+use tokio::runtime::Builder;
 
 pub async fn start(config: server::Config) {
+    // get physical cpu count and divide by 2.
+    // one half is given to tokio runtime and the another to rayon thread pool.
+    // the rayon threadpool worker count acan also be set via config/flag
+    let physical_cores = num_cpus::get_physical();
+    let half_physical_cores = (physical_cores / 2).max(1);
+
+    let tokio_runtime = Builder::new_multi_thread()
+        .worker_threads(half_physical_cores)
+        .max_blocking_threads(50)
+        .enable_all()
+        .build()
+        .expect("Failed to create Tokio runtime");
+
+    tracing::info!(
+        "Tokio runtime configured with {} worker threads ⚙️",
+        half_physical_cores
+    );
+
     // print terminal art
     println!("{}", server::ART);
 
     // init port number
-    let port = config.port.unwrap_or(3000);
+    let http_port = config.port.unwrap_or(3000);
+    let grpc_port = config.port.unwrap_or(4000);
 
     // setup shared state
-    let shared_state = match build_app_state_from_config(config.clone()).await {
+    let shared_state = match build_app_state(config.clone(), half_physical_cores).await {
         Ok(state) => state,
         Err(e) => {
             tracing::error!(
@@ -25,12 +45,19 @@ pub async fn start(config: server::Config) {
         }
     };
 
-    if config.protocol == HTTP {
-        http::server::start(shared_state, port).await.expect("")
-    } else {
-        // start grpc server
-        grpc::server::start(shared_state, port).await.expect("")
-    }
+    tokio_runtime.block_on(async move {
+        if config.protocol == HTTP {
+            // Start HTTP server
+            http::server::start(shared_state, http_port)
+                .await
+                .expect("Failed to start HTTP server");
+        } else {
+            // Start gRPC server
+            grpc::server::start(shared_state, grpc_port)
+                .await
+                .expect("Failed to start gRPC server");
+        }
+    });
 }
 
 #[cfg(test)]
